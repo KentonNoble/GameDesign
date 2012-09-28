c35860426a59b7d602ae49e807979fb6502e328e
James MacKay <jamesmackay10011@gmail.com>
2012-09-27 23:41:18 -0500

    Version0.1
    Updated Creature Maker Constructor
    Allows for non-writable variables
    Cleaner

--- GameApplicationJavaScript/JavaScript/CreatureTools/CreatureMaker.js
+++ GameApplicationJavaScript/JavaScript/CreatureTools/CreatureMaker.js
@@ -10,84 +10,56 @@ function Creature(I, N, A, D, AT) {
         return new Creature(I, N, A, D, AT);
     }
 
-    var ID = I, name = N, atk = A, def = D, attack_type = AT, involved = true, cool_down = 1, instance_count = 0, creatureSprite;
-
-
-    //Used to determine casting cost of creature
-    //formula:(atk*2 + this.def + num attacks) *coolDown
-    var cost = function () {
-        var counter = 0, x;
-        for (x = 0; x < 5; x += 1) {
-            if (attack_type[2][x] === 1) {
-                counter += 1;
-            }
+    Object.defineProperties(this, {
+        "ID"            :{ get : function(){return I } },
+        "Name"          :{ get : function(){return N } },
+        "Atk"           :{ get : function(){return A } },
+        "Def"           :{ get : function(){return D } },
+        "AttackType"    :{ get : function(){return AT } },
+        "Cost"          :{ get : function(){
+                                            var counter = 0, x;
+                                            for (x = 0; x < 5; x += 1) {
+                                                if (this.AttackType[2][x] === 1) {
+                                                    counter += 1;
+                                                }
+                                            }
+                                            return (((this.Atk * 2) + this.Def + (counter * 2)));
         }
-        return (((atk * 2) + def + (counter * 2)) * cool_down);
-    };
+        },
+        "DamageGrid"    :{ get : function () {
+                                                var damageGrid = [[],[],[],[],[]], x, y;
 
-    //Damage grid is used in damage calculations
-    var creature_damage_grid = function () {
-        var damageGrid = [], z, x, y;
 
-        for (z = 0; z < 5; z += 1) {
-            damageGrid[z] = [];
+
+                                                for (x = 0; x < 5; x += 1) {
+                                                    for (y = 0; y < 5; y += 1) {
+                                                        damageGrid[x][y] = this.AttackType[x][y] * this.Atk;
+                                                    }
+                                                }
+                                                return damageGrid;
+                                                }
         }
+    });
 
-        for (x = 0; x < 5; x += 1) {
-            for (y = 0; y < 5; y += 1) {
-                damageGrid[x][y] = attack_type[x][y] * atk;
-            }
-        }
-        return damageGrid;
-    };
+    var Involved,
+        CoolDown,
+        InstanceCount;
 
-    //getters
-    this.getID = function() {
-        return ID;
-    };
-    this.getName = function() {
-        return name;
-    };
-    this.getAtk = function() {
-        return atk;
-    };
-    this.getDef = function() {
-        return def;
-    };
-    this.getAttack_Type = function() {
-        return attack_type;
-    };
-    this.getInvolved = function() {
-        return involved;
-    };
-    this.getCreatureSprite = function() {
-        return creatureSprite;
-    };
-    this.getCool_Down = function() {
-        return cool_down;
-    };
-    this.getInstance_Count = function() {
-        return instance_count;
-    };
-    this.getCost = function() {
-        return cost;
-    };
-    this.getCreature_Damage_Grid = function() {
-        return creature_damage_grid;
-    };
+    Object.defineProperties(this,{
+        "Involved"      :{ get : function(){return Involved} ,
+                           set : function (InvolvedVariable) {
+                                 Involved = InvolvedVariable
+                         }
+        },
+        "CoolDown"      :{ get : function(){ return CoolDown},
+                           set : function (CoolDownVariable) {
+                           CoolDown = CoolDownVariable;
+                          }
+        },
+        "InstanceCount":{ get : function(){ return InstanceCount},
+                          set : function (IntanceVariable) {
+                          InstanceCount = IntanceVariable;
+                         }}
 
-
-    //setters(only those needed)
-    this.setInvolved = function(x) {
-        involved = x;
-    };
-
-    this.setCool_Down = function(x) {
-        cool_down = x;
-    };
-
-    this.setInstanceCount = function(x) {
-        instance_count = x;
-    }
-
+    });
 }
